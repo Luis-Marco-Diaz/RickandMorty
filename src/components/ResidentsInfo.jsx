a5ac59f3470e2c2300e4c08dc47055dbcae11574
Marco Diaz <123221592+Luis-Marco-Diaz@users.noreply.github.com>
2023-05-23 15:09:55 +0000

    Update App.css and ResidentsInfo.jsx

--- src/components/ResidentsInfo.jsx
+++ src/components/ResidentsInfo.jsx
@@ -1,11 +1,13 @@
 const ResidentsInfo = ( { residentData } ) => {
     return (
         <div className="resident-card">
-            <h3>{ residentData.name }</h3>
+            <h3 className="names">{ residentData.name }</h3>
             <img src={ residentData.image } alt="" />
+            <div className="info">
             <p><b> Status: { residentData.status }</b></p>
             <p><b> Origin Name: { residentData.origin.name }</b></p>
             <p><b> Episodes Appears: { (residentData.episode).length }</b></p>
+            </div>
         </div>
     )    
 };
